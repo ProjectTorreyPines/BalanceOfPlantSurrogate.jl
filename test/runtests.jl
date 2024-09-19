00ca90ef7d25a3c6bbc2a8fdd193a760362cb87f
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2024-09-19 16:53:21 -0700

    add test!

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using BalanceOfPlantSurogate
+
+@testset "BalanceOfPlantSurogate" begin
+    for cycle in [:rankine, :brayton]
+        BOPSur = BalanceOfPlantSurogate.BOPSurogate(cycle)
+        @assert !isnan(BalanceOfPlantSurogate.predict_thermal_efficiency(BOPSur,1e7,0.9,0.1))
+    end
+end
